46c6d71c980a6fe780d7924d29592ea9e41252a6
progmamun <mamun333089@gmail.com>
2022-05-21 20:42:27 +0600

    initial-setup

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  'mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8tqhr.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('').collection('');
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello From assignment-12!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
