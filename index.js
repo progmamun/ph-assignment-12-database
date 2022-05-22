a5b7a05b1a03be578a0a3f7b3eeba2f03eaa0600
progmamun <mamun333089@gmail.com>
2022-05-22 21:39:02 +0600

    nodemon

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8tqhr.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8tqhr.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
