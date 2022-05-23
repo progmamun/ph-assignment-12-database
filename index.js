30de5023ac42c03414ea21b37e43a32cb0cab58a
progmamun <mamun333089@gmail.com>
2022-05-23 11:49:51 +0600

    verifyAdmin-api

--- index.js
+++ index.js
@@ -39,16 +39,46 @@ async function run() {
     const serviceCollection = client.db('manufacturer').collection('products');
     const userCollection = client.db('manufacturer').collection('users');
 
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === 'admin') {
+        next();
+      } else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    };
+
     // product api
     app.get('/product', async (req, res) => {
       const products = await serviceCollection.find({}).toArray();
       res.send(products);
     });
 
-    // users api
+    // user api
     app.get('/user', verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
+    });
+
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
+    });
+
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     app.put('/user/:email', async (req, res) => {
@@ -63,7 +93,7 @@ async function run() {
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '1h' }
+        { expiresIn: '1d' }
       );
       res.send({ result, token });
     });
