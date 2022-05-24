d0f01fb4611e8351311e7b52e93eca3707691acd
progmamun <mamun333089@gmail.com>
2022-05-24 06:59:33 +0600

    product-delete-api

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       res.send(product);
     });
 
+    // DELETE
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // user api
     app.get('/user', verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
