5ccdc7fbdb5f023e6ef964e697b42297fb984089
progmamun <mamun333089@gmail.com>
2022-05-26 15:59:51 +0600

    booking-delete-api

--- index.js
+++ index.js
@@ -231,6 +231,13 @@ async function run() {
       res.send({ success: true, result });
     });
 
+    app.delete('/booking/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // payment api stripe
     app.patch('/booking/:id', verifyJWT, async (req, res) => {
       const id = req.params.id;
