958cfd5209b82afec6e29a8451acdd8637b11b66
progmamun <mamun333089@gmail.com>
2022-05-29 14:18:20 +0600

    update-api

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const client = new MongoClient(uri, {
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.sendStatus(401).send({ message: 'UnAuthorized access' });
+    return res.status(401).send({ message: 'UnAuthorized access' });
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      res.status(401).send({ message: 'Forbidden access' });
+      return res.status(403).send({ message: 'Forbidden access' });
     }
     req.decoded = decoded;
     next();
