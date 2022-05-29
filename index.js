f54e828c7da3bde1956cdf4f72f26cf2de57c381
progmamun <mamun333089@gmail.com>
2022-05-29 10:40:15 +0600

    jwt

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthorized access' });
+    return res.sendStatus(401).send({ message: 'UnAuthorized access' });
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
