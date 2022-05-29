d2d812533598c38a6c3050b3a59299e766d583c3
progmamun <mamun333089@gmail.com>
2022-05-29 15:19:57 +0600

    update-api

--- index.js
+++ index.js
@@ -99,6 +99,12 @@ async function run() {
       res.send(users);
     });
 
+    app.get('/user/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      res.send(user);
+    });
+
     // user update api
     app.put('user/:email', async (req, res) => {
       const email = req.params.email;
